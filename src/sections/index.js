d9be89fb3e199f4c724cf0498bed9aede17fb357
mozmmil-ali <mly88207@gmail.com>
2024-04-24 15:22:15 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export { SuperQuality, Subscribe, SpecialOffer, Footer, Services, CustomerReviews, PopularProducts, Hero };
